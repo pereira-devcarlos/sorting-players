8f4aeaaaa688e84a5b87c584b2667c84e0a21017
pereira-devcarlos <contato.devcarlos@gmail.com>
2025-11-26 13:08:14 -0300

    Adiciona arquivos de implementação e estrutura inicial do projeto

---
+++ src/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "leitura.h"
+#include "algoritmos.h"
+
+int main(int argc, char const *argv[]){
+    // Implemetar a lógica da função principal
+    
+    return 0;
+}
