d2c708f2f32667071fa5b9414224dc2209decb1f
pereira-devcarlos <contato.devcarlos@gmail.com>
2025-12-08 19:09:02 -0300

    Adiciona teste para a função carregar_jogadores e exibe informações dos jogadores carregados

--- src/main.c
+++ src/main.c
@@ -5,6 +5,23 @@
 #include "algoritmos.h"
 
 int main(int argc, char const *argv[]){
+    // Testando função de carregar jogadores
+    Jogador *jogadores = NULL;
+    int n = carregar_jogadores("data/jogadores.csv", &jogadores);
 
+    if (n <= 0) {
+        printf("Erro ao carregar jogadores.\n");
+        return 1;
+    }
+
+    
+    for (int i = 0; i < n; i++) {
+        printf("%s (%d)\n", jogadores[i].nome, jogadores[i].idade);
+    }
+    
+    printf("Carregados %d jogadores.\n", n);
+    
+    free(jogadores);
+    
     return 0;
 }
