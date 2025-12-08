0d354c680c1599978a8ff4c1f285deaa797f4501
pereira-devcarlos <contato.devcarlos@gmail.com>
2025-12-08 19:35:23 -0300

    Refactor code structure for improved readability and maintainability

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 #include "leitura.h"
 #include "algoritmos.h"
 
@@ -10,17 +11,23 @@ int main(int argc, char const *argv[]){
     int n = carregar_jogadores("data/jogadores.csv", &jogadores);
 
     Metricas m;
-    radixSortNomes(jogadores, n, &m);
 
-    salvar_jogadores("data/saida.txt", jogadores, n);
+    clock_t inicio = clock();
+    radixSortNomes(jogadores, n, &m);
+    clock_t fim = clock();
+
+    double tempo_ms = (double)(fim - inicio) / CLOCKS_PER_SEC * 1000.0;
+
+    salvar_jogadores("data/jogadores_ordenados.csv", jogadores, n);
 
     for(int i = 0; i < n; i++){
         printf("%s, %s, %s, %s, %d\n", jogadores[i].nome, jogadores[i].posicao, jogadores[i].naturalidade, jogadores[i].clube, jogadores[i].idade);
     }
 
+    printf("Tempo de execucao: %.2f(ms)\n", tempo_ms);
     printf("Comparacoes: %ld\n", m.comparacoes);
     printf("Trocas: %ld\n", m.trocas);
-    printf("Memoria: %ld\n", m.memoria);
+    printf("Memoria: %ld(bytes)\n", m.memoria);
     
     free(jogadores);
 
